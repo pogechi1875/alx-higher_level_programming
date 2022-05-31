413a89121b245e9f86b984ddf9b95038a4efe94d
pogechi1875 <ogp1875@gmail.com>
2022-05-31 13:27:18 -0700

    Python loops

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-if number == 0:
-    print(f"{number} number is zero")
-elif number > 0:
-    print(f"{number} number is positive")
+if number > 0:
+    print(number, "is positive")
+elif number < 0:
+    print(number, " is negative")
 else:
-    print(f"{number} number is negative")
+    print(number. "is zero")
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+digit = abs(number) % 10
 if number < 0:
-    last  = number % -10
-else:
-    last = number % 10
-
-if last  > 5:
-    print(f"Last digit of {number} is {last} and is greater than 5")
-elif last < 6 and last != 0:
-    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
-else:
-    print(f"Last digit of {number} is {last} and is 0")
+    digit = -digit
+print('Last digit of {:d} is {:d} '.format(number, digit), end='')
+if digit > 5:
+    print('and is greater than 5')
+elif digit == 0:
+    print('and is 0')
+else digit < 6:
+    print('and is less than 6 and not 0')
