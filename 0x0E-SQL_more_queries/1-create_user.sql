6a8e5fbfd1274ce9dda9525b6fdf1fca7079ab51
pogechi1875 <ogp1875@gmail.com>
2022-08-12 18:15:53 -0700

    Corrections done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
---used to create a user with certain permissions
+-- Creates the user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
