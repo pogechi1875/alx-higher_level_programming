9fcbbe4e0aee8e4b46481d53d43a8bdc398a4b22
Peter Ogechi <artemis@DESKTOP-B7EL5T0.localdomain>
2022-09-05 23:52:13 +0300

    Javascript prints

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* Script that prints threee lines of code */
+
+const c = 'C is fun';
+const p = 'Python is cool';
+const j = 'Javascript is amaizing';
+
+console.log(c);
+console.log(p);
+console.log(j);
