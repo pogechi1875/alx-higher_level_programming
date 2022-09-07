203ceb2978eedc5c10c4ae0761ac31816ca37d26
Peter Ogechi <artemis@DESKTOP-B7EL5T0.localdomain>
2022-09-07 23:13:47 +0300

    tasks for javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const arg = process.argv.slice(2);
-if (arg.length == 0 ) {
-	console.log('No argument')
-}else if(arg.length == 2){
-	console.log('Argument found')
-}else{
-	console.log('Argument found')
+if (arg.length === 0) {
+  console.log('No argument');
+} else if (arg.length === 2) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
 }
--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,4 +2,4 @@
 const argv1 = process.argv[2];
 const argv2 = process.argv[3];
 
-console.log(argv1 + ' is ' + argv2)
+console.log(argv1 + ' is ' + argv2);
