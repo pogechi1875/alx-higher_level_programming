91fd3844eebe589c3aa07005f0accfcc74058069
Peter Ogechi <artemis@DESKTOP-B7EL5T0.localdomain>
2022-10-24 15:03:04 +0300

    Fixed some errors

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.8
+
 if __name__ == "__main__":
-    import hidden_4
-    for i in names:
+      import hidden_4
+      names = dir(hidden_4)
+      for i in names:
         if i[:2] != "__":
-            print(f"{i}"
+            print(f"{i}")
